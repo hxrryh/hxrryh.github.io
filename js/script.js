773370c883c122e3b895905ae11098b50f8aa4e9
hxrryh <pd.frisk@gmail.com>
2018-01-24 16:47:50 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+particlesJS.load('particle', 'js/config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
+
+window.onload = function(e){ 
+    startTime();
+}
